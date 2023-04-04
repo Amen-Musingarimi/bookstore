a34e176123ab2457faa38ff58cd9dde5c367bb60
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-04-04 15:52:34 +0200

    Add book and remove book functionality

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,32 +1,45 @@
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/booksSlice';
 
-const BookDetails = ({ category, title, author }) => (
-  <div className="book-details">
-    <div>
-      <h4 className="category-name">{category}</h4>
-      <h3 className="book-title">{title}</h3>
-      <p className="author-name">{author}</p>
+const BookDetails = ({
+  category, author, title, id,
+}) => {
+  const dispatch = useDispatch();
+
+  const deleteBookHandler = () => {
+    dispatch(removeBook({ id }));
+  };
+
+  return (
+    <div className="book-details">
+      <div>
+        <h4 className="category-name">{category}</h4>
+        <h3 className="book-title">{title}</h3>
+        <p className="author-name">{author}</p>
+      </div>
+      <div className="action-buttons">
+        <button type="button" className="actionBtn">
+          Comments
+        </button>
+        <div className="vertical-line" />
+        <button type="button" className="actionBtn" onClick={deleteBookHandler}>
+          Remove
+        </button>
+        <div className="vertical-line" />
+        <button type="button" className="actionBtn">
+          Edit
+        </button>
+      </div>
     </div>
-    <div className="action-buttons">
-      <button type="button" className="actionBtn">
-        Comments
-      </button>
-      <div className="vertical-line" />
-      <button type="button" className="actionBtn">
-        Remove
-      </button>
-      <div className="vertical-line" />
-      <button type="button" className="actionBtn">
-        Edit
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 BookDetails.propTypes = {
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default BookDetails;
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,11 +1,36 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
 import './styles/BookForm.css';
 
 const BookForm = () => {
-  const [selectedOption, setSelectedOption] = useState('Action');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Action');
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const dispatch = useDispatch();
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const bookSubmitHandler = (event) => {
+    event.preventDefault();
+    dispatch(addBook({
+      title, author, category, id: uuidv4(),
+    }));
+    setTitle('');
+    setAuthor('');
+    setCategory('Action');
   };
 
   return (
@@ -16,11 +41,21 @@ const BookForm = () => {
           placeholder="Book title"
           type="text"
           className="book-title-input"
+          value={title}
+          onChange={handleTitleChange}
+          required
+        />
+        <input
+          placeholder="Author"
+          type="text"
+          className="book-author-input"
+          value={author}
+          onChange={handleAuthorChange}
         />
         <select
           id="dropdown"
-          value={selectedOption}
-          onChange={handleOptionChange}
+          value={category}
+          onChange={handleCategoryChange}
           className="category-input"
         >
           <option value="Action" className="category">
@@ -29,11 +64,27 @@ const BookForm = () => {
           <option value="Science Fiction" className="category">
             Science fiction
           </option>
-          <option value="Economy" className="category">
-            Economy
+          <option value="Non-fiction" className="category">
+            Non-fiction
+          </option>
+          <option value="Romance" className="category">
+            Romance
+          </option>
+          <option value="Mystery" className="category">
+            Mystery
+          </option>
+          <option value="Horror" className="category">
+            Horror
+          </option>
+          <option value="Academics" className="category">
+            Academics
           </option>
         </select>
-        <button type="button" className="add-book-btn">
+        <button
+          type="button"
+          className="add-book-btn"
+          onClick={bookSubmitHandler}
+        >
           ADD BOOK
         </button>
       </form>
